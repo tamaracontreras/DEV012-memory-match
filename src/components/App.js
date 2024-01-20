6b2a52e795b402e39cbc6f84825d14aa3e1faaaa
Tam <tacs.laboral@gmail.com>
2024-01-20 01:04:30 -0300

    renrizacion 18 cards

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ import { Vikingos } from '../data/vikings/vikings.js';
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
-//
+
 // fetch('./data/pokemon/pokemon.json')
 //   .then(resp => resp.json())
 //   .then(console.log)
@@ -20,60 +20,48 @@ import { Vikingos } from '../data/vikings/vikings.js';
 //
 
 const App = () => {
+  
   const el = document.createElement('div');
-    el.classList.add('contenedor');
-    
-
+  el.classList.add('contenedor');
+ // ota forma es hacer una copia del array con 
+ // Vikingos.items = [...Vikingos.items, ...Vikingos.items];
+  
+    for (let i = 0; i < 2; i++) {
     Vikingos.items.forEach(vikingo => {
-        // Crea un contenedor general
-        const cardContainer = document.createElement('div');
-        cardContainer.classList.add('card-container');
+     
+// console.log(Vikingos.items.length)
 
-        // Crea un contenedor para la imagen
-        const imgContainerDiv = document.createElement('div');
-        imgContainerDiv.classList.add('img-container');
+  // Crea un contenedor general
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('card-container');
 
-        // Crea el elemento de imagen
-        const imgElement = document.createElement('img');
-        imgElement.setAttribute('src', vikingo.image);
-        imgElement.setAttribute('alt', vikingo.id);
-        imgElement.setAttribute('id', 'imagen');
+ // Crea un contenedor para la imagen
+    const imgContainerDiv = document.createElement('div');
+    imgContainerDiv.classList.add('img-container');
 
-        // Crea el contenedor de información
-        const infoContainerDiv = document.createElement('div');
-        infoContainerDiv.classList.add('info-container');
+ // Crea el elemento de imagen
+    const imgElement = document.createElement('img');
+    imgElement.setAttribute('src', vikingo.image);
+    imgElement.setAttribute('alt', vikingo.id);
+    imgElement.setAttribute('id', 'imagen');
 
-        // Crea el elemento de descripción
+ // Crea el contenedor de información
+    const infoContainerDiv = document.createElement('div');
+    infoContainerDiv.classList.add('info-container');
+
     
+  el.appendChild(cardContainer);
+  cardContainer.appendChild(imgContainerDiv);
+  imgContainerDiv.appendChild(imgElement)
 
-        // Crea el elemento de nombre
-        // const nombre = document.createElement('div');
-        // nombre.classList.add('nombre');
-        // nombre.textContent = vikingo.id;
-
-        // Agrega la imagen al contenedor de la imagen
-
-
-        // Agrega todos los elementos al contenedor de información
-        el.appendChild(cardContainer);
-        cardContainer.appendChild(imgContainerDiv);
-        imgContainerDiv.appendChild(imgElement);
- 
-        // imgContainerDiv.appendChild(nombre);
-      
-
-        // Agrega el contenedor de imagen y el contenedor de información al contenedor general
-       
-
-        // Agrega el contenedor general al DOM
       
     });
-
+  }
     return el;
 };
- 
 
-
+    
+  
 
 
 export default App;
--- src/data/vikings/vikings.js
+++ src/data/vikings/vikings.js
@@ -4,7 +4,7 @@ export  const Vikingos = {
     { id: 'martillo_de_thor', image: 'https://th.bing.com/th/id/OIG.Uwj8XkY8awr5SuU1zwZy?pid=ImgGn', bgColor: '#339933' },
     { id: 'iyggdrasil', image:' https://th.bing.com/th/id/OIG.18LdhjUHvxouPdmd2h8r?pid=ImgGn', bgColor: '#339933' },
     { id: 'lanza_de_odin', image: 'https://th.bing.com/th/id/OIG.aFqlJ.7y6i95vVwDE6Qb?pid=ImgGn', bgColor: '#339933' },
-    { id: 'amuleto_mjolnir', image: 'https://www.bing.com/images/create/martillo-de-thor-ilustracion-antigua/1-65a82b5663ce46cb876bcce60b178533?id=XPBO2mnu7bBImFMDT14pDQ%3d%3d&view=detailv2&idpp=genimg&FORM=GCRIDP&mode=overlay', bgColor: '#F05032' },
+    { id: 'valkiria', image: 'https://th.bing.com/th/id/OIG.Uv8fj.U8g2CJ81G7KEtr?pid=ImgGn', bgColor: '#F05032' },
     { id: 'barco_drakkar', image: 'https://th.bing.com/th/id/OIG.21vLKzVgU1YIFM5f1Pk1?pid=ImgGn', bgColor: '#F05032' },
     { id: 'lobo_fenrir', image: 'https://th.bing.com/th/id/OIG.xY2lC4ekj7dqnTWYs5q6?pid=ImgGn', bgColor: '#F05032' },
     { id: 'espada_vikinga', image: 'https://th.bing.com/th/id/OIG.zuEDpzstBDuxjp_S0SrL?pid=ImgGn', bgColor: '#1572B6' },
