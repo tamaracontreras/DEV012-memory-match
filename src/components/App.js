2f36ede2d0a1510fb535c59408493e2465efd57d
Tam <tacs.laboral@gmail.com>
2024-02-01 20:16:28 -0300

    funcionalidad de mostrar, ocultar y contador

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
-
-
-
-
 // Para incluir los diferentes sets de cartas podemos _importar_ el archivo
 // JavasSript que contenga el `export` correspondiente...
 //
 // import { PokemonData } from '../data/pokemon/pokemon.js';
-import { Vikingos } from '../data/vikings/vikings.js';
-import { shuffleArray } from '../main.js';
-import {cambiarEstilosCarta } from '../main.js';
+import { Vikingos } from "../data/vikings/vikings.js";
+import { mostrarCarta, shuffleArray, ocultarCarta} from "../main.js";
 
+
+// import { buscarCartaPorId } from "../main.js";
 
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
@@ -19,91 +16,136 @@ import {cambiarEstilosCarta } from '../main.js';
 //   .then(resp => resp.json())
 //   .then(console.log)
 //   .catch(console.error);
-//
 
-const App = () => {
- 
-  const contenedor_principal = document.createElement('div');
-   contenedor_principal.classList.add('contenedor');
+export const App = () => {
+  let cartaGuardada = "";
+  let cartaGuardadaDos = "";
+  let objetoGuardado = "";
+  let objetoGuardadoDos = "";
+  let puntos = 0;
+  alert('Haz click para empezar a Jugar!');
 
-   const contenedorboton = document.createElement('div');
-   contenedorboton.classList.add('contenedor_boton');
-   
-   const boton = document.createElement('button');
-    boton.classList.add('revolver');
-    boton.textContent = 'Shuffle';
-// esta es la funcion que se encarga de cambiar si se esconde o se 
-// muestra la imagen ya sea del objeto(imgContainerDiv) o bien la del dorso
-   
- // otra forma es hacer una copia del array con 
- // Vikingos.items = [...Vikingos.items, ...Vikingos.items];
- function barajarCartas() {
-  shuffleArray(Vikingos.items);
- 
-  contenedor_principal.innerHTML = '';
-    for (let i = 0; i < 2; i++) {
-    Vikingos.items.forEach(vikingo => {
+  const contenedor_principal = document.createElement("div");
+
+  contenedor_principal.classList.add("contenedor");
+  
+  const nuevoH1 = document.createElement("h1");
+  nuevoH1.classList.add('puntuacion');
+  nuevoH1.textContent = "Puntuación: " + puntos;
+
+
+  const contenedorboton = document.createElement("div");
+  contenedorboton.classList.add("contenedor_boton");
+
+  const boton = document.createElement("button");
+  boton.classList.add("revolver");
+  boton.textContent = "Shuffle";
+  boton.addEventListener("click", barajarYCrearCartas);
+
+  function barajarYCrearCartas() {
+    Vikingos.copia = [...Vikingos.items, ...Vikingos.items];
+    shuffleArray(Vikingos.copia);
+    renderizarCartas();
+  }
+
+  function renderizarCartas() {
+    contenedor_principal.innerHTML = "";
+    shuffleArray(Vikingos.copia);
+    for (let i = 0; i < 18; i++) {
+      // Dentro del bucle, se crea un contenedor para cada carta,
+      // se establecen las propiedades de la imagen y se agregan
+      //  al contenedor principal.
+      // Crea un contenedor general
+      const cardContainer = document.createElement("div");
+      cardContainer.classList.add("card-container");
+      // Crea un contenedor para la dorso imagen
+      const containerDorso = document.createElement("div");
+      containerDorso.classList.add("dorso-container");
+      containerDorso.style.display = "flex";
+
+      // Crea el elemento de imagen de Dorso
+      const imgDorso = document.createElement("img");
+      imgDorso.classList.add("imagenDorso");
+      imgDorso.setAttribute("src", "../imagenes/dorso.jpg");
+      imgDorso.setAttribute("alt", "rubik");
+
+      // Crea un contenedor para la imagen
+      const imgContainerDiv = document.createElement("div");
+      imgContainerDiv.classList.add("img-container");
+      imgContainerDiv.style.display = "none";
+
+      // Crea el elemento de imagen
+      const imgElement = document.createElement("img");
+      imgElement.setAttribute("src", Vikingos.copia[i].image);
+      imgElement.setAttribute("alt", Vikingos.copia[i].id);
+      imgElement.setAttribute("id", "imagen");
+
+      contenedor_principal.appendChild(cardContainer);
+      contenedor_principal.appendChild(nuevoH1);
+
+      cardContainer.appendChild(imgContainerDiv);
+      imgContainerDiv.appendChild(imgElement);
+
+      cardContainer.appendChild(containerDorso);
+      containerDorso.appendChild(imgDorso);
+
+      contenedor_principal.appendChild(contenedorboton);
+      contenedorboton.appendChild(boton);
+
+      // Agrega el evento de clic a cada carta
+
+      // cardContainer.addEventListener("click", cambiarEstilosCarta);
+
+      cardContainer.addEventListener("click", function (e) {
       
-     
-// console.log(Vikingos.items.length)
 
-// Dentro del bucle, se crea un contenedor para cada carta,
-// se establecen las propiedades de la imagen y se agregan
-//  al contenedor principal.
+        if (cartaGuardada === "") {
+          cartaGuardada = Vikingos.copia[i].id;
+          // guarda el objeto seleccionado del primer click (cardContainer) aplicando la funcion mostrarCarta
+          objetoGuardado = mostrarCarta(e);
 
-  // Crea un contenedor general
-    const cardContainer = document.createElement('div');
-    cardContainer.classList.add('card-container');
-     // Crea un contenedor para la dorso imagen
-     const containerDorso = document.createElement('div');
-     containerDorso.classList.add('dorso-container');
-     containerDorso.style.display = 'flex';
- 
-  // Crea el elemento de imagen de Dorso
-     const imgDorso = document.createElement('img');
-     imgDorso.setAttribute('src','../imagenes/dorso.jpg');
-     imgDorso.setAttribute('alt', 'rubik');
-     imgDorso.setAttribute('id', 'imagenDorso');
+          console.log(objetoGuardado);
+          console.log("cartauno");
+          // con el return termina la ejecucion de esta funcion
+          return;
+
+        }
+        
+        if (cartaGuardadaDos === "") {
+          cartaGuardadaDos = Vikingos.copia[i].id;
+          objetoGuardadoDos = mostrarCarta(e);
+          console.log(objetoGuardadoDos);
+          console.log("cartados");
+        }
 
 
- // Crea un contenedor para la imagen
-    const imgContainerDiv = document.createElement('div');
-    imgContainerDiv.classList.add('img-container');
-    imgContainerDiv.style.display = 'none';
+        if (cartaGuardada !== cartaGuardadaDos) {
+          console.log("Son distintos!");
+          setTimeout(function () {
+          ocultarCarta(objetoGuardado);
+          ocultarCarta(objetoGuardadoDos)}, 2000);
 
- // Crea el elemento de imagen
-    const imgElement = document.createElement('img');
-    imgElement.setAttribute('src', vikingo.image);
-    imgElement.setAttribute('alt', vikingo.id);
-    imgElement.setAttribute('id', 'imagen');
+        } else{
+          puntos = puntos + 1
+          nuevoH1.textContent = "Puntuación: " + puntos;
+          //puntos++
+          //puntos+=1
+        }
+          cartaGuardada = "";
+          cartaGuardadaDos = "";
+          
+
    
 
-  contenedor_principal.appendChild(cardContainer);
-
-  cardContainer.appendChild(imgContainerDiv);
-  imgContainerDiv.appendChild(imgElement);
-  cardContainer.appendChild(containerDorso);
-  containerDorso.appendChild(imgDorso);
-  contenedor_principal.appendChild(contenedorboton);
-  contenedorboton.appendChild(boton)
- // Agrega el evento de clic a cada carta
- cardContainer.addEventListener('click', cambiarEstilosCarta);
-  
-    });
-    
+        //comparar ambas cartas
+        //paso dos : si son iguales mostrar las imagenes
+        //si son diferentes dejarlas ocultas
+        //resetear los valores de las cartaGuardada ='', cartaGuardadaDos ='',
+        
+      });
+    }
   }
-  }
-   // Agrega el evento de clic al botón
-   boton.addEventListener('click', barajarCartas);
+  barajarYCrearCartas();
 
-   // Baraja las cartas inicialmente
-   barajarCartas();
-    return contenedor_principal;
+  return contenedor_principal;
 };
-
-    
-  
-
-
-export default App;
-
--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -1,8 +1,27 @@
 import App from './App.js';
 
+
 describe('App', () => {
   it('should render without crashing', () => {
-    const el = App();
-    expect(el instanceof HTMLElement).toBe(true);
+    const {contenedor_principal} = App();
+    expect(contenedor_principal instanceof HTMLElement).toBe(true);
   });
 });
+
+
+//
+// Para incluir los diferentes sets de cartas podemos _importar_ el archivo
+// JavasSript que contenga el `export` correspondiente...
+//
+// import pokemon from '../data/pokemon/pokemon.js';
+// console.log(pokemon);
+//
+// O alternativamente podríamos cargar el JSON de forma asíncrona usando
+// `fetch` en el momento que consideremos necesario.
+//
+// fetch('./data/pokemon/pokemon.json')
+//   .then(resp => resp.json())
+//   .then(console.log)
+//   .catch(console.error);
+//
+
