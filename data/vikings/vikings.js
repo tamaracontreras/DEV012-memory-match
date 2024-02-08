ad6155de5c278a8525f4dad4d952148de0991869
Tam! <102377583+tamaracontreras@users.noreply.github.com>
2024-02-08 16:10:23 -0300

    Update vikings.js

--- data/vikings/vikings.js
+++ data/vikings/vikings.js
@@ -1,14 +1,14 @@
 export  const Vikingos = {
   name: 'Vikings',
   items: [
-    { id: 'martillo_de_thor', image: '../Imagenes/martillo_de_thor.jpg', bgColor: '#339933' },
-    { id: 'iyggdrasil', image:'../Imagenes/yggdrasil.jpg', bgColor: '#339933' },
-    { id: 'lanza_de_odin', image:'../Imagenes/lanza_de_odin.jpg', bgColor: '#339933' },
-    { id: 'valkiria', image: '../Imagenes/valkiria.jpg', bgColor: '#F05032' },
-    { id: 'barco_drakkar', image: '../Imagenes/barco_drakkar.jpg', bgColor: '#F05032' },
-    { id: 'lobo_fenrir', image: '../Imagenes/lobo_fenrir.jpg', bgColor: '#F05032' },
-    { id: 'espada_vikinga', image: '../Imagenes/espada_vikinga.jpg', bgColor: '#1572B6' },
-    { id: 'escritura_runica', image: '../Imagenes/escritura_runica.jpg', bgColor: '#1572B6' },
-    { id: 'vegvísir', image: '../Imagenes/vegvísir.jpg', bgColor: '#1572B6' },
+    { id: 'martillo_de_thor', image: './Imagenes/martillo_de_thor.jpg', bgColor: '#339933' },
+    { id: 'iyggdrasil', image:'./Imagenes/yggdrasil.jpg', bgColor: '#339933' },
+    { id: 'lanza_de_odin', image:'./Imagenes/lanza_de_odin.jpg', bgColor: '#339933' },
+    { id: 'valkiria', image: './Imagenes/valkiria.jpg', bgColor: '#F05032' },
+    { id: 'barco_drakkar', image: './Imagenes/barco_drakkar.jpg', bgColor: '#F05032' },
+    { id: 'lobo_fenrir', image: './Imagenes/lobo_fenrir.jpg', bgColor: '#F05032' },
+    { id: 'espada_vikinga', image: './Imagenes/espada_vikinga.jpg', bgColor: '#1572B6' },
+    { id: 'escritura_runica', image: './Imagenes/escritura_runica.jpg', bgColor: '#1572B6' },
+    { id: 'vegvísir', image: './Imagenes/vegvísir.jpg', bgColor: '#1572B6' },
   ],
 };
